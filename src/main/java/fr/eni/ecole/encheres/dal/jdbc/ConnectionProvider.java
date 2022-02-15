bae2116397f1b89c803d6b371cb557297d27b9d2
Samuel Louerat <samuel.louerat2021@campus-eni.fr>
2022-02-15 12:42:34 +0100

    creation de la  connexion à la base de donnée

---
+++ src/main/java/fr/eni/ecole/encheres/dal/jdbc/ConnectionProvider.java
@@ -0,0 +1,36 @@
+package fr.eni.ecole.encheres.dal.jdbc;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+import fr.eni.ecole.encheres.dal.DALException;
+
+public class ConnectionProvider {
+	
+	/**
+	 * Obtenir une connexion ouverte vers la base de données
+	 * @return
+	 * @throws DALException
+	 */
+	public static Connection getConnection() throws DALException {
+		Connection cnx = null;
+		try {
+			Context context =  new InitialContext();
+			DataSource datasource = (DataSource) context.lookup("java:comp/env/jdbc/pool_cnx_encheresBDD");
+			cnx = datasource.getConnection();
+		} catch (NamingException e) {
+			DALException exception = new DALException("Le pool de connexions est introuvable !");
+			throw exception;
+		} catch (SQLException e) {
+			DALException exception = new DALException("Problème de connexion à la base de données ! " + e.getMessage());
+			throw exception;
+		}
+		return cnx;
+	}
+
+}
