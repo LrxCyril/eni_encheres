719d64d145be521a6f30744f8492062985d32f52
Cyril Leroux <cyril.leroux.ca@gmail.com>
2022-02-15 12:54:58 +0100

    création de la BLLException

---
+++ src/main/java/fr/eni/ecole/encheres/bll/BLLException.java
@@ -0,0 +1,24 @@
+package fr.eni.ecole.encheres.bll;
+
+public class BLLException extends Exception {
+
+	public BLLException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public BLLException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public BLLException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public BLLException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
