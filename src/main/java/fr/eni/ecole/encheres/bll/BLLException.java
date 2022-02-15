e48cbf80d480407fdac3f958fccf4c087cc84dd6
Samuel Louerat <samuel.louerat2021@campus-eni.fr>
2022-02-15 15:54:29 +0100

    Exception ajout des constantes predefinies

--- src/main/java/fr/eni/ecole/encheres/bll/BLLException.java
+++ src/main/java/fr/eni/ecole/encheres/bll/BLLException.java
@@ -2,6 +2,11 @@ package fr.eni.ecole.encheres.bll;
 
 public class BLLException extends Exception {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public BLLException() {
 		// TODO Auto-generated constructor stub
 	}
