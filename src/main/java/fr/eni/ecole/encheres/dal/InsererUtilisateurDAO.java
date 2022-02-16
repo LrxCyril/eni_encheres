71bf3fed804e5ecb5d7416e9a5678e8ef29b1c62
Teddy Verre <teddy.verre2021@campus-eni.fr>
2022-02-16 11:24:31 +0100

    Return int

--- src/main/java/fr/eni/ecole/encheres/dal/InsererUtilisateurDAO.java
+++ src/main/java/fr/eni/ecole/encheres/dal/InsererUtilisateurDAO.java
@@ -3,7 +3,7 @@ package fr.eni.ecole.encheres.dal;
 import fr.eni.ecole.encheres.bo.Utilisateur;
 
 public interface InsererUtilisateurDAO {
-	Void VerifIdentifiantExistant(String email,String pseudo) throws DALException ;
+	int VerifIdentifiantExistant(String email,String pseudo) throws DALException ;
 	Utilisateur InsertUtilisateur(Utilisateur nouvelUtilisateur) throws DALException;
 
 	
