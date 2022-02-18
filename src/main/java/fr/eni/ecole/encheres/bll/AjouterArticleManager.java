287c1b779fc11522ee70a8e1aaa2667908e241cb
Teddy Verre <teddy.verre2021@campus-eni.fr>
2022-02-18 15:25:19 +0100

    Rien

--- src/main/java/fr/eni/ecole/encheres/bll/AjouterArticleManager.java
+++ src/main/java/fr/eni/ecole/encheres/bll/AjouterArticleManager.java
@@ -2,6 +2,13 @@ package fr.eni.ecole.encheres.bll;
 
 public class AjouterArticleManager {
 	
-
+inserartcile(artcile)
 	
+method jdbc insert article 
+
 }
+
+faire inserer utilisateur  utilisateurmanager
+
+
+meme chose jdbc 
