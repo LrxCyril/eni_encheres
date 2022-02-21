0261ebab0650c4a30cbdf2ec5f5678bfdefacb15
Cyril Leroux <cyril.leroux.ca@gmail.com>
2022-02-21 11:21:50 +0100

    création ArticleVendu.java : création de la classe

---
+++ src/main/java/fr/eni/ecole/encheres/bo/ArticleVendu.java
@@ -0,0 +1,119 @@
+package fr.eni.ecole.encheres.bo;
+
+import java.time.LocalDateTime;
+
+public class ArticleVendu {
+	
+	private int noArticle;
+	private String nomArticle;
+	private String description;
+	private LocalDateTime dateDebutEncheres;
+	private LocalDateTime dateFinEncheres;
+	private int miseAPrix;
+	private int prixVente;
+	private int etatVente;
+	private int no_categorie;
+	private Categorie categorieArticle;
+	private Retrait lieuRetrait;
+	private Utilisateur utilisateur;
+	
+	
+	// --- Getters and Setters ----------------------
+	
+	// --- numero de l'article
+	public int getNoArticle() {
+		return noArticle;
+	}
+	public void setNoArticle(int noArticle) {
+		this.noArticle = noArticle;
+	}
+	
+	// --- nom de l'article
+	public String getNomArticle() {
+		return nomArticle;
+	}
+	public void setNomArticle(String nomArticle) {
+		this.nomArticle = nomArticle;
+	}
+	
+	// --- description
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	// --- date de début des enchères
+	public LocalDateTime getDateDebutEncheres() {
+		return dateDebutEncheres;
+	}
+	public void setDateDebutEncheres(LocalDateTime dateDebutEncheres) {
+		this.dateDebutEncheres = dateDebutEncheres;
+	}
+	
+	// ---  date de fin des enchères
+	public LocalDateTime getDateFinEncheres() {
+		return dateFinEncheres;
+	}
+	public void setDateFinEncheres(LocalDateTime dateFinEncheres) {
+		this.dateFinEncheres = dateFinEncheres;
+	}
+	
+	// --- mise à prix
+	public int getMiseAPrix() {
+		return miseAPrix;
+	}
+	public void setMiseAPrix(int miseAPrix) {
+		this.miseAPrix = miseAPrix;
+	}
+	
+	// --- prix de vente
+	public int getPrixVente() {
+		return prixVente;
+	}
+	public void setPrixVente(int prixVente) {
+		this.prixVente = prixVente;
+	}
+	
+	// --- état de la vente
+	public int getEtatVente() {
+		return etatVente;
+	}
+	public void setEtatVente(int etatVente) {
+		this.etatVente = etatVente;
+	}
+	
+	// --- numéro catégorie
+	public int getNo_categorie() {
+		return no_categorie;
+	}
+	public void setNo_categorie(int no_categorie) {
+		this.no_categorie = no_categorie;
+	}
+	
+	// --- categorie de l'article
+	public Categorie getCategorieArticle() {
+		return categorieArticle;
+	}
+	public void setCategorieArticle(Categorie categorieArticle) {
+		this.categorieArticle = categorieArticle;
+	}
+	
+	// --- lieu de retrait
+	public Retrait getLieuRetrait() {
+		return lieuRetrait;
+	}
+	public void setLieuRetrait(Retrait lieuRetrait) {
+		this.lieuRetrait = lieuRetrait;
+	}
+	
+	// --- utilisateur
+	public Utilisateur getUtilisateur() {
+		return utilisateur;
+	}
+	public void setUtilisateur(Utilisateur utilisateur) {
+		this.utilisateur = utilisateur;
+	}
+
+}
