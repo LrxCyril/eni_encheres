8105b9b4f7c853855a428c0e52ff462e54b9a5ab
Teddy Verre <teddy.verre2021@campus-eni.fr>
2022-02-21 10:59:46 +0100

    Création des constantes SQL_INSERT_ARTICLE Création des constantes SQL_DELETE_ARTICLE

--- src/main/java/fr/eni/ecole/encheres/dal/jdbc/ArticleDAOJdbcImpl.java
+++ src/main/java/fr/eni/ecole/encheres/dal/jdbc/ArticleDAOJdbcImpl.java
@@ -5,7 +5,10 @@ import fr.eni.ecole.encheres.dal.ArticleDAO;
 import fr.eni.ecole.encheres.dal.DALException;
 
 public class ArticleDAOJdbcImpl implements ArticleDAO {
-	//creer constate de requete d'insertion d'un article
+	//creer constante de requete d'insertion d'un article
+	private static final String SQL_INSERT_ARTICLE = "INSERT INTO ARTICLES_VENDUS (nom_article,description,date_debut_encheres,date_fin_encheres,prix_initial,prix_vente,no_utilisateur,no_categorie VALUES (?,?,?,?,?,?,?,?)";
+	private static final String SQL_DELETE_ARTICLE = "DELETE FROM ARTICLES_VENDUS WHERE no_article=?";
+	
 	@Override
 	public void InsertArticles(Article nouvelArticle) throws DALException {
 		//creer commande SQL inserer article (idem inserer utilisateur dans Utilisateur DAO JdBC Impl
