5d9c77d19606df7146f17c45f60a7b0e788c346f
Teddy Verre <teddy.verre2021@campus-eni.fr>
2022-02-22 16:18:37 +0100

    Ajout HttpSession session = request.getSession(); 			ajoutArticle.setNoUtilisateur((int) session.getAttribute("idUtilisateur"));

--- src/main/java/fr/eni/ecole/encheres/controllers/AjouterArticleServlet.java
+++ src/main/java/fr/eni/ecole/encheres/controllers/AjouterArticleServlet.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.sql.Date;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -11,10 +13,12 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import fr.eni.ecole.encheres.bll.ArticleManager;
 import fr.eni.ecole.encheres.bll.BLLException;
 import fr.eni.ecole.encheres.bo.Article;
+import fr.eni.ecole.encheres.bo.Categorie;
 import fr.eni.ecole.encheres.dal.DALException;
 
 /**
@@ -23,15 +27,14 @@ import fr.eni.ecole.encheres.dal.DALException;
 @WebServlet("/ajout/article")
 public class AjouterArticleServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
-	private ArticleManager mgr;
+	private ArticleManager manager =new ArticleManager();
 	
     /**
      * @see HttpServlet#HttpServlet()
      */
     public AjouterArticleServlet() {
         super();
-        mgr = new ArticleManager();
+        //mgr = new ArticleManager();
        
     }
 
@@ -39,7 +42,18 @@ public class AjouterArticleServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		
+		List<Categorie> categorieArticle =new ArrayList<Categorie>();
+		try {
+			categorieArticle= manager.selectCategorie();
+			System.out.println(categorieArticle.get(0).getLibelle());
+			
+			request.setAttribute("listeCategories", categorieArticle);
+		} catch (BLLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/ajout_article.jsp");
 		rd.forward(request, response);
 	}
@@ -52,11 +66,12 @@ public class AjouterArticleServlet extends HttpServlet {
 		
 			Article ajoutArticle = new Article();
 			Boolean vide = false;
-			
+			HttpSession session = request.getSession();
+			ajoutArticle.setNoUtilisateur((int) session.getAttribute("idUtilisateur"));
 			ajoutArticle.setNomArticle(request.getParameter("nom_article"));
 			
 			ajoutArticle.setDescription(request.getParameter("description"));
-			ajoutArticle.setNoCategorie(Integer.parseInt(request.getParameter("no_categorie")));
+			ajoutArticle.setNoCategorie(Integer.parseInt(request.getParameter("categories")));
 			//Photo article
 			//TODO
 			DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
@@ -69,7 +84,7 @@ public class AjouterArticleServlet extends HttpServlet {
 			
 			//envoyer article fonctionBLL
 			try {
-				mgr.insertArticles(ajoutArticle);
+				manager.insertArticles(ajoutArticle);
 			} catch (DALException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
