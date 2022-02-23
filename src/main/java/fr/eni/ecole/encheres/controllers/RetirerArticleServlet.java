03c28fb2fa0a4499e6e6db94dd1a785ac48844c9
Teddy Verre <teddy.verre2021@campus-eni.fr>
2022-02-23 10:15:24 +0100

    Création/Modification REquestDIspatcher => /ajout_article.jsp

--- src/main/java/fr/eni/ecole/encheres/controllers/RetirerArticleServlet.java
+++ src/main/java/fr/eni/ecole/encheres/controllers/RetirerArticleServlet.java
@@ -1,6 +1,8 @@
 package fr.eni.ecole.encheres.controllers;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -19,23 +21,24 @@ public class RetirerArticleServlet extends HttpServlet {
      */
     public RetirerArticleServlet() {
         super();
-        // TODO Auto-generated constructor stub
+
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/ajout_article.jsp");
+		rd.forward(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		
+		
 	}
 
 }
