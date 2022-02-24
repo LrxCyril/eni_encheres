cd46e012970d574e0507bb9768bbe948d7b79a94
Samuel Louerat <samuel.louerat2021@campus-eni.fr>
2022-02-24 09:45:17 +0100

    Afficher le detail d'une enchere (champs prerenmpli)

--- src/main/java/fr/eni/ecole/encheres/dal/jdbc/EnchereDAOJdbcImpl.java
+++ src/main/java/fr/eni/ecole/encheres/dal/jdbc/EnchereDAOJdbcImpl.java
@@ -24,8 +24,10 @@ public class EnchereDAOJdbcImpl implements EnchereDAO {
 	private static final String SQL_SELECT_ENCHERE = "SELECT UTILISATEURS.no_utilisateur, credit, prix_initial, prix_vente FROM ARTICLES_VENDUS INNER JOIN UTILISATEURS ON UTILISATEURS.no_utilisateur = ARTICLES_VENDUS.no_utilisateur WHERE no_article = ? ";
 	private static final String SQL_SELECT_ENCHERE_EXIST = "SELECT TOP 1 no_enchere, date_enchere, montant_enchere,ENCHERES.no_article, ENCHERES.no_utilisateur, credit, ARTICLES_VENDUS.prix_initial, ARTICLES_VENDUS.prix_vente FROM ENCHERES inner Join UTILISATEURS on ENCHERES.no_utilisateur =UTILISATEURS.no_utilisateur inner Join ARTICLES_VENDUS on ENCHERES.no_article=ARTICLES_VENDUS.no_article WHERE ENCHERES.no_article=? ORDER BY montant_enchere DESC;";
 	private static final String SQL_INSERT_ENCHERE = "INSERT INTO ENCHERES (date_enchere,montant_enchere,no_article,no_utilisateur) values (?,?,?,?)"; // première enchère
-	private static final String SQL_ENCHERE_COMPLETE="SELECT TOP 1  UTILISATEURS.no_utilisateur as [no_encherisseur],UTILISATEURS.pseudo as [encherisseur],no_enchere,nom_article, description, date_fin_encheres, montant_enchere,prix_initial, ENCHERES.no_article ,(SELECT pseudo from ARTICLES_VENDUS inner Join UTILISATEURS on ARTICLES_VENDUS.no_utilisateur=UTILISATEURS.no_utilisateur where no_article=?) as [vendeur],(SELECT telephone from ARTICLES_VENDUS inner Join UTILISATEURS on ARTICLES_VENDUS.no_utilisateur=UTILISATEURS.no_utilisateur where no_article=?)as [tel_vendeur],(SELECT Utilisateurs.no_utilisateur from ARTICLES_VENDUS inner Join UTILISATEURS on ARTICLES_VENDUS.no_utilisateur=UTILISATEURS.no_utilisateur where no_article=?)as[no_vendeur],RETRAITS.code_postal,RETRAITS.rue,RETRAITS.ville FROM ENCHERES inner Join UTILISATEURS on ENCHERES.no_utilisateur =UTILISATEURS.no_utilisateur inner Join ARTICLES_VENDUS on ENCHERES.no_article=ARTICLES_VENDUS.no_article inner Join RETRAITS on ARTICLES_VENDUS.no_article=RETRAITS.no_article WHERE ENCHERES.no_article=? ORDER BY montant_enchere DESC;";
+	private static final String SQL_ENCHERE_COMPLETE_OLD="SELECT TOP 1  UTILISATEURS.no_utilisateur as [no_encherisseur],UTILISATEURS.pseudo as [encherisseur],no_enchere,nom_article, description, date_fin_encheres, montant_enchere,prix_initial, ENCHERES.no_article ,(SELECT pseudo from ARTICLES_VENDUS inner Join UTILISATEURS on ARTICLES_VENDUS.no_utilisateur=UTILISATEURS.no_utilisateur where no_article=?) as [vendeur],(SELECT telephone from ARTICLES_VENDUS inner Join UTILISATEURS on ARTICLES_VENDUS.no_utilisateur=UTILISATEURS.no_utilisateur where no_article=?)as [tel_vendeur],(SELECT Utilisateurs.no_utilisateur from ARTICLES_VENDUS inner Join UTILISATEURS on ARTICLES_VENDUS.no_utilisateur=UTILISATEURS.no_utilisateur where no_article=?)as[no_vendeur],RETRAITS.code_postal,RETRAITS.rue,RETRAITS.ville FROM ENCHERES inner Join UTILISATEURS on ENCHERES.no_utilisateur =UTILISATEURS.no_utilisateur inner Join ARTICLES_VENDUS on ENCHERES.no_article=ARTICLES_VENDUS.no_article inner Join RETRAITS on ARTICLES_VENDUS.no_article=RETRAITS.no_article WHERE ENCHERES.no_article=? ORDER BY montant_enchere DESC;";
 	private static final String SQL_UPDATE_ENCHERE = null; // surenchérir
+	private static final String SQL_ENCHERE_COMPLETE="SELECT (Select TOP 1 ENCHERES.no_enchere  FROM ENCHERES  where ENCHERES.no_article=? ORDER BY ENCHERES.montant_enchere DESC)as[no_enchere],(Select TOP 1 ENCHERES.montant_enchere  FROM ENCHERES  where ENCHERES.no_article=? ORDER BY ENCHERES.montant_enchere DESC)as[montant_enchere],(Select TOP 1 UTILISATEURS.no_utilisateur  FROM ENCHERES inner join UTILISATEURS on ENCHERES.no_utilisateur=UTILISATEURS.no_utilisateur  where ENCHERES.no_article=? ORDER BY ENCHERES.montant_enchere DESC)as[no_encherisseur],(Select TOP 1 UTILISATEURS.pseudo  FROM ENCHERES inner join UTILISATEURS on ENCHERES.no_utilisateur=UTILISATEURS.no_utilisateur  where ENCHERES.no_article=? ORDER BY ENCHERES.montant_enchere DESC)as[encherisseur],UTILISATEURS.no_utilisateur as [no_vendeur],UTILISATEURS.pseudo as [vendeur],telephone as [tel_vendeur],nom_article,ARTICLES_VENDUS.no_article, description, date_fin_encheres,prix_initial,RETRAITS.code_postal,RETRAITS.rue,RETRAITS.ville FROM ARTICLES_VENDUS inner Join UTILISATEURS on ARTICLES_VENDUS.no_utilisateur =UTILISATEURS.no_utilisateur  inner Join RETRAITS on ARTICLES_VENDUS.no_article=RETRAITS.no_article WHERE ARTICLES_VENDUS.no_article=?;";
+
 	
 
 	@Override
@@ -41,30 +43,35 @@ public class EnchereDAOJdbcImpl implements EnchereDAO {
 			PreparedStatement ordre = cnx.prepareStatement(SQL_ENCHERE_COMPLETE);
 		
 		// --- 3 | Ajouter le paramètre à la requête (Where...)
+
 			ordre.setInt(1, noArticle);
 			ordre.setInt(2, noArticle);
 			ordre.setInt(3, noArticle);
 			ordre.setInt(4, noArticle);
+			ordre.setInt(5, noArticle);
 		
 		// --- 4 | Appeler la méthode construisant l'enchère
 			ResultSet rs = ordre.executeQuery(); 
-		
 		// --- 5 | Vérifier si la connexion est existante
 			if (rs.next()) {
 				Utilisateur encherisseur = new Utilisateur();
 				encherisseur.setNoUtilisateur(rs.getInt("no_encherisseur"));
 				encherisseur.setPseudo(rs.getString("encherisseur"));
+System.out.println("renseigement utilisateur");
 				Utilisateur vendeur = new Utilisateur();
 				vendeur.setNoUtilisateur(rs.getInt("no_vendeur"));
 				vendeur.setPseudo(rs.getString("vendeur"));
 				vendeur.setTelephone(rs.getString("tel_vendeur"));
+System.out.println("renseignement vendeur");
 				Enchere enchere = new Enchere();
 				enchere.setNoEnchere(rs.getInt("no_enchere"));
 				enchere.setMontantEnchere(rs.getInt("montant_enchere"));
+System.out.println("renseignement retrait");
 				Retrait retrait = new Retrait();
 				retrait.setCodePostal(rs.getString("code_postal"));
 				retrait.setRue(rs.getString("rue"));
 				retrait.setVille(rs.getString("ville"));
+System.out.println("renseignement retrait");
 				Article article = new Article();
 				article.setNoCategorie(rs.getInt("no_article"));
 				article.setNomArticle(rs.getString("nom_article"));
@@ -77,7 +84,7 @@ public class EnchereDAOJdbcImpl implements EnchereDAO {
 				enchereComplete.setRetrait(retrait);
 				enchereComplete.setArticle(article);
 				enchereComplete.setVendeur(vendeur);
-								
+System.out.println("construction objet");								
 			}
 			
 		} catch (SQLException e) {
